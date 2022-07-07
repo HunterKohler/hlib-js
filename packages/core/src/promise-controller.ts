7621aa6001fca372e77026f622f93f0404904fa8
John Hunter Kohler <jhunterkohler@gmail.com>
2022-07-06 22:48:15 -0400

    refactored core promise-controller base impl

--- packages/core/src/promise-controller.ts
+++ packages/core/src/promise-controller.ts
@@ -1,38 +1,42 @@
+class PromiseControllerBase<T> {
+    public readonly promise: Promise<T>;
+    public readonly resolve!: (value: T | PromiseLike<T>) => void;
+    public readonly reject!: (reason: unknown) => void;
+    public resolved = false;
+    public rejected = false;
+
+    public constructor() {
+        this.promise = new Promise((resolve, reject) => {
+            (this.resolve as any) = resolve;
+            (this.reject as any) = reject;
+        });
+    }
+}
+
 export class PromiseController<T> {
-    private readonly _promise: Promise<T>;
-    private readonly _resolve!: (value: T | PromiseLike<T>) => void;
-    private readonly _reject!: (reason: unknown) => void;
-    private _resolved = false;
-    private _rejected = false;
+    private readonly _base = new PromiseControllerBase<T>();
+
+    public get promise() {
+        return this._base.promise;
+    }
 
     public get resolved() {
-        return this._resolved;
+        return this._base.resolved;
     }
 
     public get rejected() {
-        return this._rejected;
-    }
-
-    public get promise() {
-        return this._promise;
-    }
-
-    public constructor() {
-        this._promise = new Promise((resolve, reject) => {
-            (this._resolve as any) = resolve;
-            (this._reject as any) = reject;
-        });
+        return this._base.rejected;
     }
 
     public resolve(value: T) {
-        this._resolved = true;
-        this._rejected = false;
-        this._resolve(value);
+        this._base.resolved = true;
+        this._base.rejected = false;
+        this._base.resolve(value);
     }
 
     public reject(reason: unknown) {
-        this._resolved = true;
-        this._rejected = true;
-        this._reject(reason);
+        this._base.resolved = true;
+        this._base.rejected = true;
+        this._base.reject(reason);
     }
 }
