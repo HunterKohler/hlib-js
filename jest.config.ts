98c04678d28865042c54b6e40ba8809c68507e97
John Hunter Kohler <jhunterkohler@gmail.com>
2022-07-07 23:44:26 -0400

    exclude test files from build in core package

--- jest.config.ts
+++ jest.config.ts
@@ -1,43 +1,20 @@
+import { Config } from "jest";
+
 /**
  * For a detailed explanation regarding each configuration property and type check, visit:
  * @see https://jestjs.io/docs/configuration
  */
-export const config = {
-    // bail: 0,
+export const config: Config = {
     clearMocks: true,
     collectCoverage: true,
-    // collectCoverageFrom: undefined,
     coverageDirectory: "coverage",
-    // coveragePathIgnorePatterns: [
-    //   "/node_modules/"
-    // ],
     coverageProvider: "v8",
     coverageReporters: ["json", "lcov", "clover"],
-    // globalSetup: undefined,
-    // globalTeardown: undefined,
-    // globals: {},
-    // moduleDirectories: [
-    //   "node_modules"
-    // ],
-    // A preset that is used as a base for Jest's configuration
+    injectGlobals: false,
     preset: "ts-jest",
-    // projects: undefined,
-    // reporters: undefined,
-    // runner: "jest-runner",
-    // setupFiles: [],
-    // setupFilesAfterEnv: [],
-    // slowTestThreshold: 5,
-    // snapshotSerializers: [],
     testEnvironment: "jest-environment-node",
-    testMatch: ["**/*.(spec|test).[jt]s?(x)"],
+    testMatch: ["**/src/*.(spec|test).[jt]s?(x)"],
     testRunner: "jest-circus/runner",
-    // transform: undefined,
-    // transformIgnorePatterns: [
-    //   "/node_modules/",
-    //   "\\.pnp\\.[^\\/]+$"
-    // ],
-    // verbose: undefined,
-    // watchPathIgnorePatterns: [],
 };
 
 export default config;
